c899daadc561705f4dfc94756d4fda6b0899d62c
mig <mig@mail.com>
2025-12-02 13:47:33 +0100

    config

---
+++ colcon_ws/src/battery/src/config.h
@@ -0,0 +1,12 @@
+#define VMIN 2.5
+#define VMAX 4.2
+#define CMAX 6.0
+#define CMIN (-35.0)
+#define R 0.020
+#define NC 2.6
+#define DISCHARGING_FILENAME "/home/mig/simulator/colcon_ws/src/battery/src/discharging_data.csv"
+#define SERIES 4
+#define PARA 6
+#define LOCATION ""
+#define SERIAL_NUMBER ""
+
